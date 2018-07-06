1c486a8270bfe49ec13400117c2151c7eef3a47e
Bryan Hermsen <b_hermse@hotmail.com>
2018-07-06 10:44:57 -0500

    WIP - add pollyfill and fix build settings for mapboxgl

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,5 +3,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 module.exports = merge(common, {
+   devtool: "eval",
+   entry: ["babel-polyfill", "./src/main.js"],
     mode: 'production',
 });
